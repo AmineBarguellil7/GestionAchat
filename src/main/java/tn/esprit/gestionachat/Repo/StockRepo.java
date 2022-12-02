176d8399454473b5286d88e3c357563f2b29d1e3
lemhafamine <aminebarguellil7@gmail.com>
2022-12-02 10:18:32 +0100

    Scheduler-JPQL

--- src/main/java/tn/esprit/gestionachat/Repo/StockRepo.java
+++ src/main/java/tn/esprit/gestionachat/Repo/StockRepo.java
@@ -2,10 +2,13 @@ package tn.esprit.gestionachat.Repo;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import tn.esprit.gestionachat.entity.Stock;
 
-
+import java.util.List;
 
 
 public interface StockRepo extends JpaRepository<Stock,Long> {
+    @Query("select s from Stock s where s.qte<s.qteMin")
+    public List<Stock> getAllConcernedStocks();
 }
--- src/main/java/tn/esprit/gestionachat/service/StockService.java
+++ src/main/java/tn/esprit/gestionachat/service/StockService.java
@@ -45,14 +45,24 @@ public class StockService  implements IStockService {
     @Scheduled(cron = "* * 22 * * *")
     @Override
     public String retrieveStatusStock() {
-        List<Stock> stocks=this.retrieveAllStocks();
+
+        //without JPQL
+        /*List<Stock> stocks=this.retrieveAllStocks();
         String res="";
         for (Stock stock:stocks) {
             if (stock.getQte()<stock.getQteMin()) {
-                res=res+stock.toString()+",";
+                res=res+stock+",";
             }
         }
+        return res;*/
+
+        //with JPQL
+        List<Stock> stocks=stockRepo.getAllConcernedStocks();
+        String res="";
+        for (Stock stock:stocks) {
+            res=res+stock+" ";
+        }
         return res;
-    }
+        }
 }
 
